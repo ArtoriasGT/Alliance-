44318f4cd4f2d217c9aca000314e2db01a2b9e3d
ArtoriasGT <56262164+ArtoriasGT@users.noreply.github.com>
2019-10-21 15:25:22 +0300

    Add files via upload

---
+++ DoubleForLife.java
@@ -0,0 +1,9 @@
+public class DoubleForLife {
+    public static void pepega (double[] array) {
+
+        Double[] result = new Double[array.length];
+        for (int i = 0; i < array.length; i++) result[i] = new Double(array[i]);
+    }
+}
+
+
