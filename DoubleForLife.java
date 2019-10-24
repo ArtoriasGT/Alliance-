0100d2818664d29906750a18dbfcfb5c31610062
ArtoriasGT <56262164+ArtoriasGT@users.noreply.github.com>
2019-10-24 15:05:57 +0300

    Delete DoubleForLife.java

--- DoubleForLife.java
+++
@@ -1,9 +0,0 @@
-public class DoubleForLife {
-    public static void pepega (double[] array) {
-
-        Double[] result = new Double[array.length];
-        for (int i = 0; i < array.length; i++) result[i] = new Double(array[i]);
-    }
-}
-
-
