d4f291577c577f3d6325f0bebf6cd7f7ea81fd19
ArtoriasGT <56262164+ArtoriasGT@users.noreply.github.com>
2019-10-24 15:05:31 +0300

    Delete BooleanForLife.java

--- BooleanForLife.java
+++
@@ -1,9 +0,0 @@
-public class BooleanForLife {
-    public static Boolean[] toObject(boolean[] array) {
-
-        Boolean[] result = new Boolean[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = new Boolean(array[i]);
-        }
-        return result;}
-}
