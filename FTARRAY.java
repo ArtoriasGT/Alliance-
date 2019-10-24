adb1940907aa6f4557b18b938ba18e2768e3acd8
ArtoriasGT <56262164+ArtoriasGT@users.noreply.github.com>
2019-10-24 15:31:12 +0300

    Add files via upload

---
+++ FTARRAY.java
@@ -0,0 +1,43 @@
+public class FTARRAY {
+    public static void main(String[] args) {
+
+
+        int pepega = 10;
+        int[] zero = new int[pepega];
+        for (int i = 0; i < pepega.length; i++) {
+            zero[i] = i / i;
+            System.out.println(zero[i]);
+        }
+    }
+
+        public static void main(String[] args) {
+            double[][] zer1 = new double[15][];
+            for (int i = 0; i < zer1.length; i++) {
+                zer1[i] = new double[15];
+            }
+
+            for (int i = 0; i < zer1.length; i++) {
+                for (int j = 0; j < zer1[i].length; j++) {
+                    System.out.println("" + i + "," + j);
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            long sen = 20;
+            long[] zer2 = new long[sen];
+            for (long i = 0; i < sen; i++) {
+                zer2[i] = i / i;
+                System.out.println(zer2[i]);
+            }
+        }
+
+
+        public static void main(String[] args) {
+            boolean[] boolArr = new boolean[true; false];
+            boolean[] g = new boolean[5];
+            g[0] = true;
+            g[-2] = false;
+            System.out.println(boolArr[g]);
+        }
+    }
