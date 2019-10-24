cd15f626ebbbbd92d92f088eb7b19659d2435e2d
ArtoriasGT <56262164+ArtoriasGT@users.noreply.github.com>
2019-10-24 15:07:48 +0300

    Delete LongForLife.java

--- LongForLife.java
+++
@@ -1,7 +0,0 @@
-public class LongForLife {
-public static Long[] toObject(long[] array) {
-
-    Long[] result = new Long[array.length];
-    for (int i = 0; i < array.length; i++) result[i] = new Long(array[i]);
-    return result;}
-}
